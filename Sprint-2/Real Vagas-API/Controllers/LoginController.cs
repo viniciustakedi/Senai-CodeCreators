7b6bef6a93ae5872c76c13d84fe1b424eb6b5ac1
viniciustakedi <viniciustakedi7@gmail.com>
2020-09-08 17:08:44 -0300

    Tipo Usuario e Login

---
+++ Sprint-2/Real Vagas-API/Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using Real_Vagas_API.ViewModels;
+
+namespace Real_Vagas_API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class LoginController : ControllerBase
+    {
+        private //Preciso do Repositorio
+        public LoginController()
+        {
+            //Preciso do repositorio
+        }
+
+        public object UsuarioDomain { get; private set; }
+
+        [HttpPost] //É um método Post
+        public IActionResult Login(LoginViewModel //Preciso do repository)
+        {
+            UsuarioDomain usuarioselecionado = //Preciso de métodos
+
+            if (UsuarioSelecionado == null)
+            {
+                return NotFound("E-mail ou senha inválidos  ");
+            }
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, usuarioselecionado.Email),
+                //preciso do usuario
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("RealVagas-key-auth"));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "Real Vagas-API",                // emissor do token
+                audience: "Real Vagas-API",              // destinatário do token
+                claims: claims,                          // dados definidos acima
+                expires: DateTime.Now.AddMinutes(60),    // tempo de expiração
+                signingCredentials: creds                // credenciais do token
+            );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
+    }
+}
---
+++ Sprint-2/Real Vagas-API/Enums/TipoUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Real_Vagas_API.Enums
+{
+    public enum TipoUsuario : int
+    {
+        Administrador = 1,
+        Empresa = 2,
+        Aluno = 3,
+        ExAluno = 4,
+        
+    }
+}
