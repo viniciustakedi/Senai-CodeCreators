969bc8de221c99a9637332cb5fd9ac3b8964e0ad
gabrielspk <gabrielsilvaferreira007@gmail.com>
2020-09-16 18:49:43 -0300

    finalizaçoes

--- Sprint-2/Real Vagas-API/Controllers/DadosController.cs
+++ Sprint-2/Real Vagas-API/Controllers/DadosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Real_Vagas_API.Domains;
 using Real_Vagas_API.Interfaces;
@@ -27,51 +28,161 @@ namespace Real_Vagas_API.Controllers
             _dado = new DadosRepository();
         }
 
+        /// <summary>
+        /// Método listar Dados
+        /// </summary>
+        /// <returns>Todos os dados</returns>
         [HttpGet]
         [Authorize(Roles = "1")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Get()
         {
+            try
+            {
+
+
             return Ok(_dado.Listar());
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
         }
 
+        /// <summary>
+        /// Buscar um dado por id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>dadoBuscado</returns>
         [HttpGet("{id}")]
         [Authorize(Roles = "3,4")]
-        public IActionResult Get(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetByid(int id)
         {
-            return Ok(_dado.BuscarPorId(id));
+            try
+            {
+
+                DbDados dadoBuscado = _dado.BuscarPorId(id);
+
+
+                if (dadoBuscado != null)
+                {
+
+                    return Ok(dadoBuscado);
+                }
+
+
+                return NotFound("Nenhum dado encontrado para o id informado");
+            }
+            catch (Exception error)
+            {
+
+                return BadRequest(error);
+            }
         }
 
+        /// <summary>
+        /// Cadastra um novo dado
+        /// </summary>
+        /// <param name="novoUsuario"></param>
+        /// <returns>NovoUsuario</returns>
         [HttpPost]
         [Authorize(Roles = "1")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Post(DbDados novoUsuario)
         {
+            try
+            {
             // Faz a chamada para o método
             _dado.Cadastrar(novoUsuario);
 
             // Retorna um status code
             return StatusCode(201);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
         }
 
+        /// <summary>
+        /// Atualiza um dado
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="dadoAtualizado"></param>
+        /// <returns>dadoAtualizado</returns>
         [HttpPut("{id}")]
         [Authorize(Roles = "3,4")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Put(int id, DbDados dadoAtualizado)
         {
-            // Faz a chamada para o método
-            _dado.Atualizar(id, dadoAtualizado);
+            try
+            {
 
-            // Retorna um status code
-            return StatusCode(204);
+                DbDados dadoBuscado = _dado.BuscarPorId(id);
+
+
+                if (dadoBuscado != null)
+                {
+
+                    _dado.Atualizar(id, dadoAtualizado);
+
+
+                    return StatusCode(204);
+                }
+
+
+                return NotFound("Nenhuma vaga encontrado para o ID informado");
+            }
+            catch (Exception error)
+            {
+
+                return BadRequest(error);
+            }
         }
+    
 
+        /// <summary>
+        /// Metodo para Deletar um dado
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Vaga Deletada</returns>
         [HttpDelete("{id}")]
         [Authorize(Roles = "1")]
-        public IActionResult Delete(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult Delete(int id)
         {
-            // Faz a chamada para o método
-            _dado.Deletar(id);
+            try
+            {
 
-            // Retorna um status code
-            return StatusCode(204);
+                DbDados dadoBuscado = _dado.BuscarPorId(id);
+
+
+                if (dadoBuscado != null)
+                {
+
+                    _dado.Deletar(id);
+
+
+                    return StatusCode(202);
+                }
+
+
+                return NotFound("Nenhum dado encontrado para o ID informado");
+            }
+            catch (Exception error)
+            {
+
+                return BadRequest(error);
+            }
         }
     }
 }
--- Sprint-2/Real Vagas-API/Controllers/UsuariosController.cs
+++ Sprint-2/Real Vagas-API/Controllers/UsuariosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Real_Vagas_API.Domains;
 using Real_Vagas_API.Interfaces;
@@ -29,22 +30,68 @@ namespace Real_Vagas_API.Controllers
         {
             _usuario = new UsuariosRepository();
         }
-
+        /// <summary>
+        /// metodo get para listar usuarios
+        /// </summary>
+        /// <returns>todos os usuarios</returns>
         [HttpGet]
         [Authorize(Roles = "1")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Get()
         {
-            return Ok(_usuario.Listar());
+            try
+            {
+                return Ok(_usuario.Listar());
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
         }
 
+        /// <summary>
+        /// Metodo Get para buscar um usuario pelo id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>usuarioBuscado</returns>
         [HttpGet("{id}")]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Get(int id)
         {
-            return Ok(_usuario.BuscarPorId(id));
+            try
+            {
+
+                DbUsuarios usuarioBuscado = _usuario.BuscarPorId(id);
+
+
+                if (usuarioBuscado != null)
+                {
+
+                    return Ok(usuarioBuscado);
+                }
+
+
+                return NotFound("Nenhum usuario encontrado para o id informado");
+            }
+            catch (Exception error)
+            {
+
+                return BadRequest(error);
+            }
         }
 
+        /// <summary>
+        /// Metodo post para cadastrar um usuario
+        /// </summary>
+        /// <param name="novoUsuario"></param>
+        /// <returns>usuarioCadastrado</returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Post(DbUsuarios novoUsuario)
         {
             if (novoUsuario.IdTipoUsuario == 3 | novoUsuario.IdTipoUsuario == 4)
@@ -61,26 +108,79 @@ namespace Real_Vagas_API.Controllers
             }
         }
 
+        /// <summary>
+        /// Metodo put para atualizar um usuario
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="usuarioAtualizado"></param>
+        /// <returns>usuarioAtualizado</returns>
         [HttpPut("{id}")]
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Put(int id, DbUsuarios usuarioAtualizado)
         {
-            // Faz a chamada para o método
-            _usuario.Atualizar(id, usuarioAtualizado);
+            try
+            {
 
-            // Retorna um status code
-            return StatusCode(204);
+                DbUsuarios usuarioBuscado = _usuario.BuscarPorId(id);
+
+
+                if (usuarioBuscado != null)
+                {
+
+                    _usuario.Atualizar(id, usuarioAtualizado);
+
+
+                    return StatusCode(204);
+                }
+
+
+                return NotFound("Nenhum usuário encontrado para o id informado");
+            }
+            catch (Exception error)
+            {
+
+                return BadRequest(error);
+            }
         }
 
+        /// <summary>
+        /// Metodo Delete para deletar um usuário
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Usuario Deletado</returns>
         [HttpDelete("{id}")]
         [Authorize(Roles = "1")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Delete(int id)
         {
-            // Faz a chamada para o método
-            _usuario.Deletar(id);
+            try
+            {
 
-            // Retorna um status code
-            return StatusCode(204);
+                DbUsuarios usuarioBuscado = _usuario.BuscarPorId(id);
+
+
+                if (usuarioBuscado != null)
+                {
+
+                    _usuario.Deletar(id);
+
+
+                    return StatusCode(202);
+                }
+
+
+                return NotFound("Nenhuma vaga encontrado para o ID informado");
+            }
+            catch (Exception error)
+            {
+
+                return BadRequest(error);
+            }
         }
     }
 }
